622041936962ef068757b45e90371b35c1b30556
f <d>
2022-03-15 19:23:15 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace PISLabVj1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Novo");
+          
         }
     }
 }
